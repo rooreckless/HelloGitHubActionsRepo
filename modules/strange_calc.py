b41908107b193a214b543171ebd3c655f3d05a40
rooreckless <rooreckless@gmail.com>
2023-05-25 16:32:04 +0900

    pytest with Actions

---
+++ modules/strange_calc.py
@@ -0,0 +1,11 @@
+#03/modules/strange_calc.py
+def add_calc_if2(a, b):
+    # if (a == 1):
+    #     b = b + 1
+    if (a==0 or (b > 0 and b < 10)):
+      print("if --true")
+    # elif (a==0):
+    #   b = b + 1
+    # else:
+    #   b = b + 1
+    return a + b
